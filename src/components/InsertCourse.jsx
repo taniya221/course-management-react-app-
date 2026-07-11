452f5036697b920ca55c04fe58779d8dc32bacf1
taniya221 <taniya.jaison007@gmail.com>
2026-07-11 14:44:22 +0530

    feature/insert added

---
+++ src/components/InsertCourse.jsx
@@ -0,0 +1,61 @@
+import React from 'react'
+
+const InsertCourse = () => {
+  return (
+    <div>
+<div className="container">
+    <div className="row">
+        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+
+<div className="row g-3">
+    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+<label htmlFor="" className="form-label">Course Name</label>
+<input type="text" className="form-control" />
+    </div>
+
+<div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+<label htmlFor="" className="form-label">Course Name</label>
+<input type="text" className="form-control" />
+    </div>
+
+<div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+<label htmlFor="" className="form-label">Duration</label>
+<input type="text" className="form-control" />
+    </div>
+   
+
+<div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+<label htmlFor="" className="form-label">Fee</label>
+<input type="text" className="form-control" />
+    </div>
+
+
+<div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+<label htmlFor="" className="form-label">Mode</label>
+<select name="" id="" className="form-control">
+    <option value="">Offline</option>
+    <option value="">Online</option>
+    <option value="">Hybrid</option>
+</select>
+
+    </div>
+    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+        <label htmlFor="" className="form-label">Trainer</label>
+        <input type="text" className="form-control" />
+    </div>
+<div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+<button className="btn btn-success">Submit</button>
+    </div>
+</div>
+        </div>
+    </div>
+</div>    
+
+    </div>
+   
+    
+  )
+}
+
+export default InsertCourse
