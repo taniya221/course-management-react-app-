cda707d874fc4f038cde37ca5e75b6272bc78e9e
taniya221 <taniya.jaison007@gmail.com>
2026-07-12 23:32:20 +0530

    insert api added

--- src/components/InsertCourse.jsx
+++ src/components/InsertCourse.jsx
@@ -1,7 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import NavigationBar from './NavigationBar'
+import axios from 'axios'
+
 
 const InsertCourse = () => {
+
+    const [input,changeInput]=useState(
+{
+
+    course_name: "",
+    duration: "",
+    fee:"",
+    mode: "",
+    trainer: ""
+}
+    )
+
+
+    const inputHandler=(event)=>{
+      changeInput({...input,[event.target.name]:event.target.value})
+    }
+   
+    const readValue=()=>{
+        console.log(input)
+
+        axios.post("https://host-demo-app.onrender.com/api/add-course",input).then(
+          (response)=>{
+            console.log(response.data)
+           
+          }
+
+        ).catch(
+            
+        )
+    }
   return (
     <div>
         <NavigationBar />
@@ -13,41 +45,46 @@ const InsertCourse = () => {
     <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
 
 <label htmlFor="" className="form-label">Course Name</label>
-<input type="text" className="form-control" />
+<input type="text" className="form-control"
+name="course_name" value={input.course_name}
+onChange={inputHandler}/>
     </div>
 
-<div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-<label htmlFor="" className="form-label">Course Name</label>
-<input type="text" className="form-control" />
-    </div>
 
 <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
 <label htmlFor="" className="form-label">Duration</label>
-<input type="text" className="form-control" />
+<input type="text" className="form-control"
+name="duration" value={input.duration} onChange={inputHandler}
+/>
     </div>
    
 
 <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
 <label htmlFor="" className="form-label">Fee</label>
-<input type="text" className="form-control" />
+<input type="text" className="form-control" 
+name="fee" value={input.fee} onChange={inputHandler}
+
+/>
     </div>
 
 
 <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
 <label htmlFor="" className="form-label">Mode</label>
-<select name="" id="" className="form-control">
-    <option value="">Offline</option>
-    <option value="">Online</option>
-    <option value="">Hybrid</option>
+<select name="" id="" className="form-control" name="mode" value={input.mode} onChange={inputHandler}>
+     <option value="">Select mode</option>
+    <option value="Offline">Offline</option>
+    <option value="Online">Online</option>
+    <option value="Hybrid">Hybrid</option>
 </select>
 
     </div>
     <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
         <label htmlFor="" className="form-label">Trainer</label>
-        <input type="text" className="form-control" />
+        <input type="text" className="form-control" 
+        name="trainer" value={input.trainer} onChange={inputHandler}/>
     </div>
-<div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-<button className="btn btn-success">Submit</button>
+<div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+<button className="btn btn-success" onClick={readValue}>Submit</button>
     </div>
 </div>
         </div>
